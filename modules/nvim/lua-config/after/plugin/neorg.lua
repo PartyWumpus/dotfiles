223d5028c35588273503963df15f1a5b7a182030
PartyWumpus <48649272+PartyWumpus@users.noreply.github.com>
2025-02-23 23:35:56 +0000

    remove old neovim config

--- modules/nvim/lua-config/after/plugin/neorg.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set("n", "<localleader>ns",
-  -- go to top of file, delete all lines below, and generate workspace summary
-  "<cmd>:1<cr><cmd>+,$d<cr><cmd>Neorg generate-workspace-summary<cr>", {
-    desc = "[Neorg] Generate summary"
-  })
-vim.keymap.set("n", "<localleader>ne", "<cmd>Neorg index<cr>", {
-  desc = "[Neorg] Enter notes"
-})
--- modules/nvim/lua-config/after/plugin/telescope.lua
+++
@@ -1,8 +0,0 @@
-require('telescope').setup {
-}
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Search filenames" })
-vim.keymap.set('n', '<leader>fs', function() -- fs = file search
-  builtin.grep_string({ search = vim.fn.input("Search: ") })
-end, { desc = "Search text in files" })
